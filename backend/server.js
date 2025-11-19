263877e156c44ed4513eb2b6df892fc172eaa5b1
Ayan <aayanpeerzade@gmail.com>
2025-11-19 13:20:41 +0530

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,13 @@ const app = express()
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: [
+    "http://localhost:3000",
+    "https://ai-music-predictor.vercel.app/"
+  ],
   credentials: true
-}))
+}));
+
 app.use(express.json())
 app.use(cookieParser())
 
